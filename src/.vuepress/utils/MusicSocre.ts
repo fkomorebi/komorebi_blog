6435c1ae5fa4dbd8ec7e938b9f53b94aaf146f9f
fkomorebi <f.komorebi@qq.com>
2024-05-25 16:59:02 +0800

    新增曲谱

--- src/.vuepress/utils/MusicSocre.ts
+++ src/.vuepress/utils/MusicSocre.ts
@@ -1,8 +1,34 @@
 const prefix = "/komorebi_blog/music_socre"
+const defaultCover = `logo.svg`
 export default [
   {
     cover: "http://y.qq.com/music/photo_new/T002R300x300M000003GDYMc4W3X5Z_2.jpg?max_age=2592000",
     title: "Covered in rain - John Mayer",
     value: `${prefix}/mayer_john-covered_in_rain.gp5`,
   },
+  {
+    cover: defaultCover,
+    title: "Belief - John Mayer",
+    value: `${prefix}/John_Mayer-Belief.gp5`
+  },
+  {
+    cover: defaultCover,
+    title: "Bold as Love - John Mayer",
+    value: `${prefix}/John_Mayer-Bold_as_Love.gp4`
+  },
+  {
+    cover: defaultCover,
+    title: "Gravity - John Mayer",
+    value: `${prefix}/John_Mayer-Gravity.gp5`
+  },
+  {
+    cover: defaultCover,
+    title: "Neon - John Mayer",
+    value: `${prefix}/John_Mayer-Neon.gp4`
+  },
+  {
+    cover: defaultCover,
+    title: "Slow Dancing in a Burning Room - John Mayer",
+    value: `${prefix}/John_Mayer-Slow_Dancing_in_a_Burning_Room.gp5`
+  }
 ]
